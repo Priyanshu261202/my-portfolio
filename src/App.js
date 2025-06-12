69c294be1ed4750b49bfd8b6aa94ea3b45a956d5
Priyanshu Shakya <priyanshushakya2612@gmail.com>
2025-06-12 16:39:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const App = () => {
             },
             move: {
               enable: true,
-              speed: 6,
+              speed: 3,
               direction: "none",
               random: false,
               straight: false,
